18291d1335dd185ef54963348e291d9ba2062523
Tim Coulson <tim@getkitt.co.uk>
2019-03-21 08:19:47 +0000

    A few tidies

--- serviceA/src/main.go
+++ serviceA/src/main.go
@@ -9,7 +9,9 @@ import (
 	"net/http"
 	"os"
 )
+
 func main() {
+	// Initialise connection with service B (ping)
 	conn, err := grpc.Dial(os.Getenv("PING_SERVICE"), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
@@ -17,6 +19,7 @@ func main() {
 	pingClient := ping.NewPingClient(conn)
 	defer conn.Close()
 
+	// HTTP endpoint to test connection
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := pingClient.Ping(context.Background(), &ping.PingRequest{Message: "ping"})
 		if err != nil {
