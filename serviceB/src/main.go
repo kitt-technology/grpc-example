e75d1aaf00f0590a7a0244af67e39813edb21f24
Tim Coulson <tim@getkitt.co.uk>
2019-05-08 09:21:52 +0100

    fix: change author

--- serviceB/src/main.go
+++ serviceB/src/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/timrcoulson/ping/proto"
+	"github.com/kitt-technology/ping/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
